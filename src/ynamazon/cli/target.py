c3fef7702e24a0170b848e1bd7922355cc46416c
Karl Wooster <karl@woostertech.com>
2025-04-26 10:07:04 -0700

    feat(partial): non-functional addition of target-orders commands

---
+++ src/ynamazon/cli/target.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from typing import Annotated, Union
+
+import typer
+from rich.console import Console
+from target_orders import get_orders
+
+app = typer.Typer(rich_markup_mode="rich")
+
+
+@app.command()
+def print_transactions(
+    cookies_path: Annotated[
+        Union[Path, None],
+        typer.Option(
+            "--cookies-path",
+            help="Path to the json file.",
+            exists=True,
+            dir_okay=False,
+        ),
+    ] = None,
+    delay: Annotated[
+        int,
+        typer.Option(
+            "--delay", help="Number of seconds to wait for Orders page to laod."
+        ),
+    ] = 5,
+    debug: Annotated[
+        bool,
+        typer.Option(
+            "--debug",
+            help="Enable debug mode.",
+            is_eager=True,
+            is_flag=True,
+        ),
+    ] = False,
+) -> None:
+    """Print transactions from the cookies file."""
+    console = Console()
+    orders = get_orders(cookies_path=cookies_path, loading_delay=delay)
+
+    console.print(orders)
